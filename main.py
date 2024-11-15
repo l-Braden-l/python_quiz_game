b907bcc2cc8529e5b208d6db617408047a28c581
braden <leachbr787@northwested.org>
2024-11-15 13:46:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,33 @@ def ask_question(question,answer):
 #Function to run the quiz 
 def run_quiz():
     questions = [
-        ('What is the fastest land animal?','cheetah')
-        ('Where is the Pyramid of Giza?','egypt')
-        ('What fish is used to make calamari?','squid')
-        ('How many Great Lakes are there?',5)
-
+        ('What is the fastest land animal?','cheetah'),
+        ('Where is the Pyramid of Giza?','egypt'),
+        ('What fish is used to make calamari?','squid'),
+        ('How many Great Lakes are there?','5')
     ]
+
+    #loop through the questions and ask each one 
+
+    score = 0 
+    for q in questions: 
+        if ask_question(q[0],q[1]):
+            print("Correct!")
+            score += 1 
+        else: 
+            print('Incorrect answer! sorry!')
+
+# calculate the score percentage 
+    print(f'\nYour final score is; {score}/{len(questions)}')
+    percentage = (score / len(questions)) * 100
+    print(f'You Scored: {percentage:.1f}%')
+    print('Goodbye!')
+
+#main function to start the game 
+def main():
+    print('Welcome to my Python Quiz Game!')
+    run_quiz()
+
+#start the game 
+if __name__ == "__main__":
+    main()
